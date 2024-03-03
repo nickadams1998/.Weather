b73e4de4a15703d27a49a8c97edaaf6b139c3bc2
nickadams1998 <161159305+nickadams1998@users.noreply.github.com>
2024-03-03 09:13:39 -1000

    New code written

--- index.js
+++ index.js
@@ -3,57 +3,57 @@ const https = require("https");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.urlencoded({extended: true}));
-
-//displays index.html of root path
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html")
+  res.sendFile(__dirname + "/index.html");
 });
 
-//invoked after hitting go in the html form
 app.post("/", function(req, res) {
-    
-    // takes in the zip from the html form, display in console. Takes in as string, ex. for zip 02139
-        var id = String(req.body.cityid);
-        console.log(req.body.cityid);
-    
-    //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
-        const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = 
-"https://api.openweathermap.org/data/2.5/weather?id=" + id + "&units=" + units + "&APPID=" + apiKey;
+  var city = String(req.body.cityInput); // Changed from zip to city
+  console.log(req.body.cityInput);
 
-    // this gets the data from Open WeatherPI
-    https.get(url, function(response){
-        console.log(response.statusCode);
-        
-        // gets individual items from Open Weather API
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const humidity = weatherData.main.humidity;
-            const speed = weatherData.wind.speed;
-            const direction = weatherData.wind.deg;
-            const cloudy = weatherData.clouds.all;
-            const city = weatherData.name;
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+  const units = "imperial";
+  const mySecret = process.env['Weather.api']; // Using environment variable for API key
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + // Changed from zip to q for query by city
+    city +
+    "&units=" +
+    units +
+    "&APPID=" +
+    mySecret;
 
-            
-            // displays the output of the results
-            res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit<h2>");
-            res.write("<h3>The humidity in " + city + " is " + humidity + " % " + " and the wind speed is " + speed + "mph " + "and the wind direction is " + direction + " degrees " + "and the cloudyness is " + cloudy + "% "+ "<h3>");
-            res.write("<img src=" + imageURL +">");
-            res.send();
-        });
+  https.get(url, function(response) {
+    console.log(response.statusCode);
+
+    let data = "";
+    response.on("data", function(chunk) {
+      data += chunk;
     });
-})
 
+    response.on("end", function() {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const city = weatherData.name;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const humidity = weatherData.main.humidity;
+      const windSpeed = weatherData.wind.speed;
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-//Code will run on 3000 or any available open port
-app.listen(process.env.PORT || 3000, function() {
-console.log ("Server is running on port")
+      res.write("<h1> The weather is " + weatherDescription + "</h1>");
+      res.write(
+        "<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit</h2>"
+      );
+      res.write("<h3>The Humidity is " + humidity + "%</h3>");
+      res.write("<h3>The Wind Speed is " + windSpeed + " MPH</h3>");
+      res.write("<img src='" + imageURL + "'>");
+      res.send();
+    });
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Server is running on port " + port);
 });
