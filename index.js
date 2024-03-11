acb73eac059e9c49bfdad42209a639ff28b44452
nickadams1998 <161159305+nickadams1998@users.noreply.github.com>
2024-03-10 22:41:10 -1000

    Update index.js
    Added Lat and Lon

--- index.js
+++ index.js
@@ -10,17 +10,20 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  var city = String(req.body.cityInput); // Changed from zip to city
-  console.log(req.body.cityInput);
+  const latitude = req.body.latitudeInput;
+  const longitude = req.body.longitudeInput;
+  console.log(latitude, longitude);
 
   const units = "imperial";
-  const mySecret = process.env['Weather.api']; // Using environment variable for API key
+  const mySecret = process.env['Weather.api']; // API key from environment variable
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" + // Changed from zip to q for query by city
-    city +
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
     "&units=" +
     units +
-    "&APPID=" +
+    "&appid=" +
     mySecret;
 
   https.get(url, function(response) {
@@ -34,25 +37,22 @@ app.post("/", function(req, res) {
     response.on("end", function() {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
-      const city = weatherData.name;
+      const city = weatherData.name; // The API will return the closest city to the lat/lon
       const weatherDescription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
-      const humidity = weatherData.main.humidity;
-      const windSpeed = weatherData.wind.speed;
+      const humidity = weatherData.main.humidity; // Extract humidity
+      const windSpeed = weatherData.wind.speed; // Extract wind speed
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
       res.write("<h1> The weather is " + weatherDescription + "</h1>");
-      res.write(
-        "<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit</h2>"
-      );
-      res.write("<h3>The Humidity is " + humidity + "%</h3>");
-      res.write("<h3>The Wind Speed is " + windSpeed + " MPH</h3>");
+      res.write("<h2>The Temperature near " + city + " is " + temp + " Degrees Fahrenheit</h2>"); // Note the text change to "near"
+      res.write("<h3>Humidity: " + humidity + "%</h3>"); // Display humidity
+      res.write("<h3>Wind Speed: " + windSpeed + " miles/hour</h3>"); // Display wind speed
       res.write("<img src='" + imageURL + "'>");
       res.send();
     });
   });
 });
-
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Server is running on port " + port);
